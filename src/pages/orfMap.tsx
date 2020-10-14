40786ca4ab5d6d94bc3a9945e3340f9dd738b94d
Ricardokinze <kinzartmusica@gmail.com>
2020-10-14 13:17:55 -0300

    12:21

--- src/pages/orfMap.tsx
+++ src/pages/orfMap.tsx
@@ -3,8 +3,19 @@ import { Link } from 'react-router-dom';
 import localImg from '../images/Local.svg';
 import { FiPlus } from 'react-icons/fi';
 import '../styles/pages/orfMap.css';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import Leaflet from 'leaflet'
+
+
+const mapIcon = Leaflet.icon({
+    iconUrl: localImg,
+
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [178, 2]
+})
+
 
 function OrphanagesMap() {
     return (
@@ -28,12 +39,20 @@ function OrphanagesMap() {
                 style={{ width: '100%', height: '100%' }}
                 >
                 <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                
+             <Marker
+                icon={mapIcon}
+                position={[-27.7630692,-48.520745]}
+                >
+                <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                Lar das meninas
+                </Popup>
+                </Marker>           
             </Map> 
 
             <Link to="/" className="create-orph">
                 <FiPlus size={32} color="#FFF" />
             </Link>
+            
         </div>
       
     );
